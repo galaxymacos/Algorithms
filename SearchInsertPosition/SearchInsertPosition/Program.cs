d0567f2f5e9feb079cddbb946c9347bebbebf133
galaxy <galaxymacos@gmail.com>
2018-05-27 20:02:11 -0400

    + SearchInsertPosition

---
+++ SearchInsertPosition/SearchInsertPosition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SearchInsertPosition
+{
+    public static class MyClass {
+        public static int SearchInsert(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+            }
+
+            return nums.Length;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = {1, 3, 5, 6};
+            Console.WriteLine(MyClass.SearchInsert(arr,5));
+        }
+    }
+}
