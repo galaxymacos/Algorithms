1faeefc30c0c0335d3926297d4f7ad0ddc07a666
galaxy <galaxymacos@gmail.com>
2018-05-27 21:38:40 -0400

    + First Bad Version

---
+++ FirstBadVersion/FirstBadVersion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    public class MyClass
+    {
+        static bool IsBadVersion(int version)
+        {
+            return (version < 10);
+        }
+
+        public static int FirstBadVersion(int n)
+        {
+            return FirstBadVersion(1, n);
+        }
+
+        public static int FirstBadVersion(int l, int r)
+        {
+            int mid = (r - l) / 2 + l;
+            if (IsBadVersion(mid))
+            {
+                if (!IsBadVersion(mid - 1) || mid == 1)
+                {
+                    return mid;
+                }
+                else
+                {
+                    return FirstBadVersion(l, mid - 1);
+                }
+            }
+            else
+            {
+                return FirstBadVersion(mid + 1, r);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(MyClass.FirstBadVersion(100));
+        }
+    }
+}
