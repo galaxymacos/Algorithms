c7f815fec88c3298cb265d5581be2650d3a81737
galaxy <galaxymacos@gmail.com>
2018-05-31 18:51:16 -0400

    + Permutation

---
+++ LongUnivaluePath/LongUnivaluePath/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LongUnivaluePath
+{
+    public class TreeNode
+    {
+        public readonly int Val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+    
+    public class FsSolution
+    {
+        public static int LongestUnivaluePath(TreeNode root)
+        {
+            int[] res = new int[1];
+            if (root != null) dfs(root, res);
+            return res[0];
+        }
+
+        private static int dfs(TreeNode root, int[] res)
+        {
+            int l = root.left != null ? dfs(root.left, res) : 0;
+            int r = root.right != null ? dfs(root.right, res) : 0;
+            int lrest = root.left!= null && root.left.Val == root.Val ? l + 1 : 0;
+            int rrest = root.right != null && root.right.Val == root.Val ? r + 1 : 0;
+            res[0] = Math.Max(res[0], lrest + rrest);
+            return Math.Max(lrest, rrest);
+        }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Permutation/Permutation/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutation
+{
+    public class MyClass
+    {
+        public static List<List<int>> Permute(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                List<List<int>> oneElementList = new List<List<int>> {new List<int> {1}};
+                return oneElementList;
+            }
+            int[] head = new int[nums.Length-1];
+            for (int i = 0; i < head.Length; i++)
+            {
+                head[i] = nums[i];
+            }
+
+            int tail = nums[nums.Length - 1];
+
+            List<List<int>> arrFromHead = Permute(head);
+            List<List<int>> result = new List<List<int>>();
+            for (int i = 0; i < arrFromHead.Count; i++)
+            {
+                for (int j = 0; j <= arrFromHead[i].Count; j++)
+                {
+                    List<int> copiedList = new List<int>();
+                    foreach (int i1 in arrFromHead[i])
+                    {
+                        copiedList.Add(i1);
+                    }
+                    copiedList.Insert(j,tail);
+                    result.Add(copiedList);
+                }
+            }
+
+            return result;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arrayA = {1,2};
+            List<List<int>> result = MyClass.Permute(arrayA);
+            foreach (var row in result)
+            {
+                foreach (int i in row)
+                {
+                    Console.Write(i+" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
