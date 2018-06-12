f3e007d46020a5f8c53cea1315d064f5f1e562c3
galaxy <galaxymacos@gmail.com>
2018-06-11 21:41:51 -0400

    + Remove Duplicate From Sorted List 2

---
+++ RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RemoveDuplicatesFromSortedList
+{
+    public class ListNode
+    {
+        public readonly int Val;
+        public ListNode Next;
+
+        public ListNode(int x)
+        {
+            Val = x;
+        }
+    }
+    
+    public class Myclass {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null)
+                return null;
+            ListNode headCopy = head;
+            while (headCopy != null)
+            {
+                ListNode endSearcher = headCopy.Next;
+                while (endSearcher != null&&headCopy.Val == endSearcher.Val)
+                {
+                    headCopy.Next = endSearcher.Next;
+                    endSearcher = endSearcher.Next;
+                    if (endSearcher == null)
+                        break;
+                }
+                headCopy = headCopy.Next;
+            }
+            
+
+            return head;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1) {Next = new ListNode(1)};
+            Myclass myclass = new Myclass();
+            myclass.DeleteDuplicates(head);
+        }
+    }
+}
---
+++ RemoveDuplicatesFromSortedList/RemoveDuplicatesFromSortedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RemoveDuplicatesFromSortedList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemoveDuplicatesFromSortedList")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("AD05C4D4-F3E5-4D7B-91D9-8D0255387C35")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
