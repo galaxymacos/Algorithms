f1f2faa74531a040345c936054f56374c02c73fc
galaxy <galaxymacos@gmail.com>
2018-06-12 19:34:57 -0400

    + Maximum Depth of Binary Tree

---
+++ MaximumDepthofBinaryTree/MaximumDepthofBinaryTree/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MaximumDepthofBinaryTree
+{
+    
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
+ 
+    public class Solution
+    {
+        private int _maxLength;
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            FindMaxDepth(root, 1);
+            return _maxLength;
+        }
+
+        private void FindMaxDepth(TreeNode root, int maxDepth)
+        {
+            if (maxDepth > _maxLength)
+                _maxLength = maxDepth;
+            if(root.left!=null)
+                FindMaxDepth(root.left,maxDepth+1);
+            if(root.right!=null)
+                FindMaxDepth(root.right,maxDepth+1);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
