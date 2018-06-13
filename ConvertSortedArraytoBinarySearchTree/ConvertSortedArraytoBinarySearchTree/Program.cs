27653fca3a2b2c29994aa251ab9c3a91eff6da9a
galaxymaxx <galaxymaxx@outlook.com>
2018-06-13 10:57:22 -0400

    Finish Convert Sorted Array to Binary Tree

--- ConvertSortedArraytoBinarySearchTree/ConvertSortedArraytoBinarySearchTree/Program.cs
+++ ConvertSortedArraytoBinarySearchTree/ConvertSortedArraytoBinarySearchTree/Program.cs
@@ -12,92 +12,23 @@ namespace ConvertSortedArraytoBinarySearchTree
   }
  
     public class Solution {
+        
+
         public TreeNode SortedArrayToBST(int[] nums)
         {
             if (nums.Length == 0)
                 return null;
             if(nums.Length == 1)
                 return new TreeNode(nums[0]);
+            if(nums.Length == 2)
+                return new TreeNode(nums[0]){right = new TreeNode(nums[1])};
             int midIndex = nums.Length / 2;
-            TreeNode head = new TreeNode(nums[midIndex]);
-            TreeNode looper = head;
-            bool isEven;
-            if (nums.Length % 2 == 0)
-                isEven = true;
-            else
-                isEven = false;
-            for (int i = midIndex-1; i >= 0; i--)
+            return new TreeNode(nums[midIndex])
             {
-                if (isEven&&nums.Length>4)
-                {
-                    
-                     looper.left = new TreeNode(nums[i-1]);
-                    looper.left.right = new TreeNode(nums[i]);
-                    isEven = false;
-                    looper = looper.left;
-                    i--;
-                }
-                else
-                {                    
-                    looper.left = new TreeNode(nums[i]);
-                    looper = looper.left;
-                }
-            }
+                left = SortedArrayToBST(nums.SubArray(0, midIndex)),
+                right = SortedArrayToBST(nums.SubArray(midIndex+1, nums.Length - midIndex-1))    // TODO check
+            };
 
-            looper = head;
-            for (int i = midIndex+1; i < nums.Length; i++)
-            {
-                looper.right = new TreeNode(nums[i]);
-                looper = looper.right;
-            }
-            
-            return head;
-        }
-
-        public TreeNode SortedArrayToBST2(int[] nums)
-        {
-            if (nums.Length == 0)
-                return null;
-            return CreateTree(nums);
-        }
-
-        private TreeNode CreateTree(int[] nums)
-        {
-            
-            if(nums.Length == 1)
-                return new TreeNode(nums[0]);
-            if (nums.Length == 0)
-                return null;
-            if (nums.Length == 2)
-            {
-                if (nums[0] > nums[1])
-                {
-                    TreeNode result = new TreeNode(nums[0]) {left = new TreeNode(nums[1])};
-                    return result;
-                }
-                else
-                {
-                    TreeNode result = new TreeNode(nums[0]) {right = new TreeNode(nums[1])};
-                    return result;
-                }
-            }
-
-//            Console.WriteLine("Index is "+(nums.Length/2+(nums.Length%2 == 1?1:0)));
-//            var array = nums.SubArray(nums.Length/2+1,nums.Length/2);
-//            Console.WriteLine("why");    // TODO
-//            foreach (var item in array)
-//            {
-//                Console.Write(item+" ");
-//            }
-
-//            Console.WriteLine();
-            TreeNode root =
-                new TreeNode(nums[nums.Length / 2])
-                {
-                    left = CreateTree(nums.SubArray(0, nums.Length / 2)),    // [-10,-3]
-                    right = CreateTree(nums.SubArray(nums.Length/2+1,nums.Length/2-(nums.Length==0?1:1)))// [5,9]
-                };
-            return root;
         }
     }
 
@@ -118,7 +49,7 @@ namespace ConvertSortedArraytoBinarySearchTree
         {
             Solution solution = new Solution();
             int[] arr = {-1,0,1,2};
-            solution.SortedArrayToBST2(arr);
+            solution.SortedArrayToBST(arr);
         }
     }
 }
