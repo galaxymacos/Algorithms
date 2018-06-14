db01392bd7c8b787b955ad9e692897c6cc830177
galaxymaxx <galaxymaxx@outlook.com>
2018-06-14 15:00:07 -0400

    + the thinking process of BestTimeToBuyAndSell

---
+++ BestTimeToBuyAndSell/BestTimeToBuyAndSell/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BestTimeToBuyAndSell
+{
+    public class Solution {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 1)
+                return prices[0];
+            return Math.Max(prices[prices.Length - 1], MaxProfit(prices.SubArray(0, prices.Length - 1)));
+        }
+
+        public int MinProfit(int[] prices)
+        {
+            if (prices.Length == 1)
+                return prices[0];
+            return Math.Min(prices[0], MinProfit(prices.SubArray(1, prices.Length - 1)));
+        }
+
+    }
+
+    public static class ArrayExtension
+    {
+        public static T[] SubArray<T>(this T[] arr, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(arr, index, result, 0, length);
+            return result;
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution mySolution = new Solution();
+            Console.WriteLine(mySolution.MaxProfit(new int[]{5,4,6,1,2}));
+            Console.WriteLine(mySolution.MinProfit(new int[]{5,4,6,1,2}));
+        }
+    }
+}
